17d7f4584f060a2c36dfb6022d2525d2ee564f4c
Stuart Leitch <stuart@stuartleitch.com>
2012-01-15 18:16:07 -0500

    Fix compiler issue and update MVC With Style to latest

--- Code/MvcFramework/MvcFramework.Mvc/Scripts/polyfill.js
+++ Code/MvcFramework/MvcFramework.Mvc/Scripts/polyfill.js
@@ -2,7 +2,7 @@
 Modernizr.load([
 {
     test: Modernizr.inputtypes && Modernizr.inputtypes.date,
-    nope: '/Scripts/jquery-ui-1.8.16.min.js', 
+    nope: '/Scripts/jquery-ui-1.8.17.min.js', 
     complete: function () {
                 $(function () {
                     $("input.date-picker").datepicker();
